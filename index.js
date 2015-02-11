9af5ae1ff7dbca61416f2eafb6ff61bb67134e09
Chris Hayen <chayen@gmail.com>
2015-02-11 17:56:01 -0500

    Progress

--- index.js
+++ index.js
@@ -1,10 +1,88 @@
 'use strict';
-module.exports = function (db) {
+
+var q = require('q');
+var mongoose = require('mongoose');
+
+var TaskModel = mongoose.model(
+  'Task',
+  {
+    name: String,
+    enabled: Boolean,
+    log: [{
+      duration: Number,
+      worked: Number,
+      date: Date
+    }]
+  }
+);
+
+module.exports = function () {
 
   return {
-    test : function() {
-      console.log(db);
+
+    createTask : function(name) {
+      var task = new TaskModel({name:name, enabled: true});
+
+      return q.Promise(function(resolve, reject) {
+        task.save(function(err, savedTask) {
+          if(err) {
+            reject(err);
+          }
+          else {
+            resolve(savedTask);
+          }
+        });
+      });
+    },
+
+    disableTask : function(task) {
+      return q.Promise(function(resolve, reject) {
+        task.enabled = false;
+        task.save(function(err, savedTask) {
+          if(err) {
+            reject(err);
+          }
+          else {
+            resolve(!savedTask.enabled);
+          }
+        });
+      });
+    },
+
+    enableTask : function(task) {
+      return q.Promise(function(resolve, reject) {
+        task.enabled = true;
+        task.save(function(err, savedTask) {
+          if(err) {
+            reject(err);
+          }
+          else {
+            resolve(savedTask.enabled);
+          }
+        });
+      });
+    },
+
+    addTime : function(task, duration, worked, date) {
+      var log = {
+        duration:duration, worked:worked, date:date
+      };
+
+      task.log.push(log);
+
+      return q.Promise(function(resolve, reject) {
+        task.save(function(err) {
+
+          if(err) {
+            reject(err);
+          }
+          else {
+            resolve(true);
+          }
+        });
+      });
     }
+
   };
 
 };
--- test/test.js
+++ test/test.js
@@ -1,19 +1,52 @@
-/*global describe, it */
+/*global describe, it, before, after */
 'use strict';
-var assert = require('assert');
+// var assert = require('assert');
 var mongoose = require('mongoose');
-var scheduler;
+var scheduler = require('../');
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+chai.should();
 
 describe('randah-scheduler node module', function () {
 
+  var task = {};
+
   before(function() {
-    mongoose.connect('mongodb://localhost/test');
-    scheduler = require('../')(mongoose.connection);
+    mongoose.connect('mongodb://localhost/scheduler-test');
   });
 
-  it('must have at least one test', function () {
-    scheduler.test();
-    assert(true, 'I was too lazy to write any tests. Shame on me.');
+  it('must create a task', function (done) {
+    scheduler().createTask('Test Task')
+    .then(function(res, err) {
+      if(err) {
+        return done(err);
+      }
+
+      task = res;
+      done();
+    });
+  });
+
+  it('must disable a task', function () {
+    return scheduler().disableTask(task)
+      .should.eventually.equal(true);
+  });
+
+  it('must enable a task', function () {
+    return scheduler().enableTask(task)
+      .should.eventually.equal(true);
+  });
+
+  it('must add time', function () {
+    return scheduler().addTime(task, 100, 50, Date.now())
+    .should.eventually.equal(true);
+  });
+
+  after(function() {
+    task.remove();
   });
 
 });
